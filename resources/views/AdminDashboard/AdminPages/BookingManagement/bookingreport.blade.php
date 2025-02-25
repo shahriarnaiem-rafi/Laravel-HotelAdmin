c3c891a4ed6c03608d3a7eccbdbb38ecd4c9a144
shahriarnaiem-rafi <shahriarnaiemrafi33.com>
2025-02-25 12:53:48 +0600

    responsive

--- resources/views/AdminDashboard/AdminPages/BookingManagement/bookingreport.blade.php
+++ resources/views/AdminDashboard/AdminPages/BookingManagement/bookingreport.blade.php
@@ -11,7 +11,7 @@
             <label for="checkout_date" class="block text-gray-700 font-semibold">Check-out Date:</label>
             <input type="date" name="checkout_date" id="checkout_date" class="py-3 px-4 bg-white border-2 border-red-500 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-black" required>
         </div>
-        <button type="submit" name="submit" id="submitbtn" class=" relative flex justify-center items-center w-full py-3 border-2 rounded-lg border-red-500 text-blue-500 font-semibold hover:text-white bg-transparent overflow-hidden group transition-all duration-500 mt-5">
+        <button type="submit" name="submit" id="submitbtn" class=" relative flex justify-center items-center w-full py-3 border-2 rounded-lg border-red-500 text-green-900 font-semibold hover:text-white bg-transparent overflow-hidden group transition-all duration-500 mt-5">
             <span class="absolute inset-0 bg-black transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-out "></span>
             <span class="relative z-10 uppercase">Search Bookings</span>
         </button>
@@ -22,19 +22,19 @@
     <div class="container mx-auto p-4">
         <div class="overflow-x-auto">
             <table class="min-w-full border border-gray-300 bg-white rounded-lg shadow-md">
-                <thead class="bg-gray-200">
+                <thead class="bg-gray-600 ">
                     <tr>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">SL</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">User Name</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">User Email</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Room Type</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Room Number</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Booking Date</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Chekin Date</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Checkout Date</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Payment</th>
-                        <th class="px-6 py-3 text-left text-gray-700 font-semibold">Total Amount</th>
-                        <!-- <th class="px-6 py-3 text-left text-gray-700 font-semibold">Actions</th> -->
+                        <th class="px-6 py-3 text-center text-white font-semibold">SL</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">User Name</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">User Email</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Room Type</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Room Number</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Booking Date</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Chekin Date</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Checkout Date</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Payment</th>
+                        <th class="px-6 py-3 text-center text-white font-semibold">Total Amount</th>
+                        <!-- <th class="px-6 py-3 text-center text-gray-700 font-semibold">Actions</th> -->
                     </tr>
                 </thead>
                 <tbody>
